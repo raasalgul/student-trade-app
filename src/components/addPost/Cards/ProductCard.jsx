69724d81355e502a7d06ca84c937419c946e20b7
raasalgul <k.sathishrsurya@gmail.com>
2021-12-16 15:23:03 +0000

    changes for product update

---
+++ src/components/addPost/Cards/ProductCard.jsx
@@ -0,0 +1,194 @@
+import { ThemeProvider,makeStyles } from '@mui/styles';
+import theme from "../../themes/Theme"
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { useEffect,useState,useContext } from 'react';
+import authHeader from '../../services/auth-header';
+import {postInfo} from "../../constants/Constant"
+import EditIcon from '@mui/icons-material/Edit';
+import { Button, Grid, IconButton, TextField } from '@mui/material';
+import { UserInfoContext } from "../../../App" 
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 444,
+      alignContent:'center'
+    },
+  });
+  
+
+export default function JobCard(props){
+
+//   const [duration,setDuration]=useState("");
+//   const [availability,setAvailability]=useState("");
+  const [picture,setPicture]=useState();
+  const [pictureData,setPictureData]=useState("");
+  const [price,setprice]=useState("");
+  const [institution,setInstitution] = useState("");
+  const [description,setDescription]=useState("");
+  const [isEdit,setIsEdit]=useState(true);
+//   const [location,setLocation] = useState("")
+  const [name,setName] =useState("");
+
+  const userInfoContext = useContext(UserInfoContext) 
+
+  const [data,setData]= useState(
+    {
+        //   "duration":"",
+          "availability":"",
+          "institution":"",
+          "picture":{data:""},
+          "price":"",
+          "description":""
+      }
+
+  );
+
+
+    async function onSaveHandler() {
+
+      let requestData={...data};
+      requestData.institution=data.institution!==institution && institution!==""?institution:data.institution;
+      requestData.email = userInfoContext.userInfoState.name;
+      requestData.name = data.name!==name && name!==""?name:data.name;
+      // requestData.picture=data.picture!==picture && area!==""?picture:data.picture;
+      requestData.picture=null;
+      requestData.price=data.price!==price && price!==""?price:data.price;
+      requestData.description=data.description!==description && description!==""?description:data.description;
+      requestData=JSON.stringify(requestData)
+      const formData = new FormData();
+      formData.append("data", requestData);
+      formData.append("file",picture);
+
+      // console.log(course)
+      console.log(requestData)
+      // requestData.phoneNumber=phoneNumber;
+      let header={...authHeader()}
+      const response = await fetch(`${postInfo}/add-oldProducts`, {
+        method: 'POST', 
+        mode: 'cors', 
+        cache: 'no-cache', 
+        credentials: 'same-origin', 
+        headers: header,
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer', 
+        body: formData 
+      });
+      return await response.json().then((val)=>{
+       // console.log(val.picture)
+      //  setPicture(val.picture)
+      setPicture(val.picture)
+       setprice(val.price)
+       setDescription(val.description)
+       setIsEdit((previous)=>!previous)
+       setData(val)
+      }).catch(()=>{
+        setIsEdit((previous)=>!previous)
+      }); 
+     }
+
+  useEffect(()=>{
+    setName(props.name) 
+    console.log(props)
+    setInstitution(props.institution)
+    setPicture(props.picture)
+    setprice(props.price)
+    setDescription(props.description)
+    // setData(props)
+     },[props]
+     )
+    const classes = useStyles();
+    return(<ThemeProvider theme={theme}>
+      { console.log(props)}
+          <Card className={classes.root} variant="outlined">
+         <CardContent>
+         <Grid alignSelf="center">
+              <Typography variant="h4">Product Update</Typography>
+              </Grid>
+           <div style={{display: 'block',marginLeft: 'auto',width: '54%'}}>
+         <Grid>
+           {isEdit?
+           <IconButton onClick={()=>{setIsEdit((previous)=>!previous)}}>
+             <EditIcon>
+               </EditIcon>
+               </IconButton>:
+               <Grid spacing={2}>
+               <Button variant="contained" onClick={onSaveHandler}
+               style={{marginRight:"8px", backgroundColor:"#2EC4B6"}}
+               >Save</Button>
+               <Button variant="contained" onClick={()=>{setIsEdit((previous)=>!previous)}}
+               style={{backgroundColor:"#2EC4B6"}}
+               >Cancel</Button>
+               </Grid>
+               }
+               </Grid>
+         </div>
+         <br/><br/>
+          <Typography variant="h5" component="h2" style={{display: 'inline-block'}} color="textSecondary">Name:</Typography>
+          {isEdit?
+          <Typography value={name} variant="h5" component="h2" style={{display: 'inline-block'}}>{name}</Typography>:
+          <TextField value={name} onChange={(e)=>{setName(e.target.value)}}></TextField>
+          }
+        <br/><br/>
+        <Typography variant="h5" component="h2" style={{display: 'inline-block'}} color="textSecondary">institution:
+          </Typography> 
+          {isEdit?
+          <Typography value={institution} variant="h5" component="h2" style={{display: 'inline-block'}}>{institution}</Typography>:
+          <TextField value={institution} onChange={(e)=>{setInstitution(e.target.value)}}></TextField>
+          }
+          <br/><br/>
+          <Grid container justifyContent="center" spacing={0}>
+        <Typography variant="h5" component="h2" style={{display: 'inline-block'}} color="textSecondary">Picture:
+          </Typography>
+          {isEdit?
+         <Grid>
+            {
+              pictureData !== null && pictureData !== undefined ?
+           <Typography variant="h5" component="h2" style={{display: 'inline-block'}}><img src={picture} alt="not Uploaded" width="100 px" height="100 px"/></Typography>
+           :
+           <Typography variant="h5" component="h2" style={{display: 'inline-block'}}>No image yet </Typography>
+            }
+         </Grid>:
+          <Grid>
+          <input
+          accept="image/*"
+          style={{ display: 'none' }}
+          id="upload-button"
+          type="file"
+          onChange={(event)=>{
+            console.log(event.target.files)
+            setPicture(event.target.files[0])
+            // setPictureData(event.target.files[0].data)
+          }}
+        />
+        <label htmlFor="upload-button">
+          <Button variant="contained" component="span" style={{backgroundColor:"#2EC4B6"}}
+          onClick={(param)=>{
+            console.log(param.target)
+          }}
+          >
+            Upload
+          </Button>
+        </label>
+        <Typography>{picture!==undefined && picture!==null?picture.name:""}</Typography>
+        </Grid> }
+        </Grid>
+          <br/><br/>
+        <Typography variant="h5" component="h2" style={{display: 'inline-block'}} color="textSecondary">price:
+          </Typography> 
+          {isEdit?
+          <Typography value={price} variant="h5" component="h2" style={{display: 'inline-block'}}>{price}</Typography>:
+          <TextField value={price} onChange={(e)=>{setprice(e.target.value)}}></TextField>
+          }
+          <br/><br/>
+        <Typography variant="h5" component="h2" style={{display: 'inline-block'}} color="textSecondary">Description:
+          </Typography>
+          {isEdit?
+          <Typography value={description} variant="h5" component="h2" style={{display: 'inline-block'}}>{description}</Typography>:
+          <TextField value={description} onChange={(e)=>{setDescription(e.target.value)}}></TextField>
+          }
+      </CardContent>
+    </Card>
+    </ThemeProvider>)
+}
---
+++ src/components/addPost/Product.jsx
@@ -0,0 +1,88 @@
+import { ThemeProvider,makeStyles } from '@mui/styles';
+import theme from "../themes/Theme"
+import { useEffect,useState,useContext } from 'react';
+import authHeader from '../services/auth-header';
+import {postInfo} from "../constants/Constant"
+import { Grid } from '@mui/material';
+import ProductCard from "./Cards/ProductCard"
+import { UserInfoContext } from "../../App"
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { Button } from '@mui/material';
+
+
+const useStyles = makeStyles((theme) => ({
+  icon:{
+    '& .MuiButton-endIcon':{
+      marginLeft: 0,
+      marginRight: 0
+    }
+  }
+  }))
+
+export default function Product(){
+  const [data,setData]= useState([]);
+  const userInfoContext = useContext(UserInfoContext)
+
+  const classes =useStyles();
+
+  useEffect(()=>{
+    // fetch(`${userInfo}/get-postInfo`,{ headers: authHeader() }).then((response) => {
+    //   return response.json();
+    // })
+    let requestData={"email":userInfoContext.userInfoState.name}
+    console.log(requestData)
+    fetch(`${postInfo}/get-postInfo`,
+    { 
+        method: 'POST', 
+      mode: 'cors', 
+      cache: 'no-cache', 
+      credentials: 'same-origin', 
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer', 
+      body: JSON.stringify(requestData) ,
+      headers: {...authHeader(),'Content-Type':'application/json'} })
+    
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((myJson) => {
+      console.log(myJson.jobTable.Items)
+      setData(myJson.jobTable.Items);
+      });
+     },[]
+     )
+     return(<ThemeProvider theme={theme}>
+        <Grid container alignItems="center" justifyContent="center" spacing={1}>
+      <Grid container alignItems="center" justifyContent="center" item xs={12} spacing={3}>
+      {
+          data.map((value)=>{
+              { console.log(value)}
+             return (<ProductCard 
+              name={value.name}
+              email={value.email}
+              institution = {value.institution}
+              description = {value.description}
+              picture = {value.picture}
+              price = {value.price}
+              />)
+          })
+            }
+               <Button variant="contained"
+          onClick={()=>{
+            let val =[... data]
+
+            let newAdd = {
+
+            }
+            val.push(newAdd)
+            setData(val)
+          }}
+          className={classes.icon}
+          style={{backgroundColor: theme.palette.secondary.main}}
+          endIcon={<AddCircleIcon />}
+          ></Button>
+        </Grid>
+         </Grid>
+        </ThemeProvider>)
+}
